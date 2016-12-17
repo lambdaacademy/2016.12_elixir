797f2432025167c606c04da4ba2d8104bedb0682
Maciej Rapacz <maciek.rapacz@gmail.com>
2016-12-17 15:09:44 +0100

    Remove printing debug messages

--- task4/iot/apps/device/lib/device/registry.ex
+++ task4/iot/apps/device/lib/device/registry.ex
@@ -1,23 +1,19 @@
 defmodule Device.Registry do
-  def start_link(options \\ []) do
-    IO.puts("Starting registry...")
-    Agent.start_link(fn -> %{} end, [options])
-	end
 
-	def add({reference, device}) do
-    IO.puts("Received a device with reference:")
-    IO.inspect(reference)
-	  Agent.update(__MODULE__, fn state -> Map.put(state, reference, device) end)
-	end
-    
-  def remove(reference) do
-    IO.puts("Removing a device:")
-    IO.inspect(reference)
-	  Agent.update(__MODULE__, fn state -> Map.delete(state, reference) end)
+  def start_link(options \\ []) do
+    Agent.start_link(fn -> %{} end, [options])
   end
-    
-	def list do
-    IO.puts("Listing devices.")
-	  Agent.get(__MODULE__, fn state -> Map.values(state) end)
-	end
+
+  def add({reference, device}) do
+    Agent.update(__MODULE__, fn state -> Map.put(state, reference, device) end)
+  end
+
+  def remove(reference) do
+    Agent.update(__MODULE__, fn state -> Map.delete(state, reference) end)
+  end
+
+  def list do
+    Agent.get(__MODULE__, fn state -> Map.values(state) end)
+  end
+
 end
--- task4/iot/apps/device/lib/device/scanner.ex
+++ task4/iot/apps/device/lib/device/scanner.ex
@@ -1,12 +1,11 @@
 defmodule Device.Scanner do
   use GenServer
+
   def start_link(options \\ []) do
     GenServer.start_link(__MODULE__, :ok, [options])
   end
     
-
   def init(:ok) do
-    IO.puts("Setting up scanner...")
     :dnssd.start
     :dnssd.browse("_http._tcp")
     {:ok, []}
@@ -14,7 +13,6 @@ defmodule Device.Scanner do
 
   def handle_info(msg, state) do
 	  {:dnssd, reference, {_, change,{name, type , domain}}} = msg
-
     case change do
       :add ->
         {:ok, {_, port, _}} = :dnssd.resolve_sync name, type , domain
@@ -28,7 +26,7 @@ defmodule Device.Scanner do
   
       :remove -> Device.Registry.remove(reference)
     end
-
     {:noreply, state}
   end
+
 end
