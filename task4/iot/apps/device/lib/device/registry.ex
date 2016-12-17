db7bbcba4140a8147d8547861181be8f7714b102
Maciej Rapacz <maciek.rapacz@gmail.com>
2016-12-17 14:56:05 +0100

    Implement registry

--- task4/iot/apps/device/lib/device/registry.ex
+++ task4/iot/apps/device/lib/device/registry.ex
@@ -1,18 +1,23 @@
 defmodule Device.Registry do
+  def start_link(options \\ []) do
+    IO.puts("Starting registry...")
+    Agent.start_link(fn -> %{} end, [options])
+	end
 
-  def list do
-    [
-      %Device{
-        name: "rpi1",
-        domain: "rpi1.local",
-        data: "port=8123"
-      },
-      %Device{
-        name: "rpi2",
-        domain: "rpi2.local",
-        data: "port=4789"
-      }
-    ]
+	def add({reference, device}) do
+    IO.puts("Received a device with reference:")
+    IO.inspect(reference)
+	  Agent.update(__MODULE__, fn state -> Map.put(state, reference, device) end)
+	end
+    
+  def remove(reference) do
+    IO.puts("Removing a device:")
+    IO.inspect(reference)
+	  Agent.update(__MODULE__, fn state -> Map.delete(state, reference) end)
   end
-
+    
+	def list do
+    IO.puts("Listing devices.")
+	  Agent.get(__MODULE__, fn state -> Map.values(state) end)
+	end
 end
