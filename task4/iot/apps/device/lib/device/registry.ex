8e7980ba6cf0dc4506d6a46d77c1d76191ee51fb
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-01-20 15:22:04 +0100

    Add (un)subscribe/1 to Device.Registry
    ToDO:
    
    - a set implementation with linear access time is a poor set implementation
    - subscribers are never removed, even if they die - this needs a monitor,
      but to handle the :DOWN message we need a GenServer not just an Agent

--- task4/iot/apps/device/lib/device/registry.ex
+++ task4/iot/apps/device/lib/device/registry.ex
@@ -11,19 +11,42 @@ defmodule Device.Registry do
 
   def add({device_id, device}) do
     Agent.update(__MODULE__, fn state ->
-      update_in(state.devices, &Map.put(&1, device_id, device))
+      new = update_in(state.devices, &Map.put(&1, device_id, device))
+      for {pid} <- new.subscribers do
+        send(pid, {:add_device, device_id, device})
+      end
+      new
     end)
   end
 
   def remove(device_id) do
     Agent.update(__MODULE__, fn state ->
-      update_in(state.devices, &Map.delete(&1, device_id))
+      new = update_in(state.devices, &Map.delete(&1, device_id))
+      for {pid} <- new.subscribers do
+        send(pid, {:remove_device, device_id})
+      end
+      new
     end)
   end
 
-  def list do
+  def list() do
     Agent.get(__MODULE__, fn state ->
       Map.values(state.devices)
+    end)
+  end
+
+  def subscribe(pid) do
+    Agent.update(__MODULE__, fn state ->
+      for {device_id, device} <- Map.to_list(state.devices) do
+        send(pid, {:add_device, device_id, device})
+      end
+      update_in(state.subscribers, &List.keystore(&1, pid, 1, {pid}))
+    end)
+  end
+
+  def unsubscribe(pid) do
+    Agent.update(__MODULE__, fn state ->
+      update_in(state.subscribers, &List.keydelete(&1, pid, 1))
     end)
   end
 
