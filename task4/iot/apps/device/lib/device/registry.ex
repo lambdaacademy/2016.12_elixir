7e3098dc6019c7e7a347e4f5996078c3e693807e
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-01-20 14:14:14 +0100

    Rewrite Device.Registry to use a State struct

--- task4/iot/apps/device/lib/device/registry.ex
+++ task4/iot/apps/device/lib/device/registry.ex
@@ -1,19 +1,30 @@
 defmodule Device.Registry do
 
+  defmodule State do
+    defstruct [devices: %{},
+               subscribers: []]
+  end
+
   def start_link(options \\ []) do
-    Agent.start_link(fn -> %{} end, [options])
+    Agent.start_link(fn -> %State{} end, [options])
   end
 
   def add({device_id, device}) do
-    Agent.update(__MODULE__, fn state -> Map.put(state, device_id, device) end)
+    Agent.update(__MODULE__, fn state ->
+      update_in(state.devices, &Map.put(&1, device_id, device))
+    end)
   end
 
   def remove(device_id) do
-    Agent.update(__MODULE__, fn state -> Map.delete(state, device_id) end)
+    Agent.update(__MODULE__, fn state ->
+      update_in(state.devices, &Map.delete(&1, device_id))
+    end)
   end
 
   def list do
-    Agent.get(__MODULE__, fn state -> Map.values(state) end)
+    Agent.get(__MODULE__, fn state ->
+      Map.values(state.devices)
+    end)
   end
 
 end
