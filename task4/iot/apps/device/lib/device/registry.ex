db1462b4cdc3e9bff24a8898dbcc53cca30e3ede
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-01-23 15:09:44 +0100

    Use a real set, not a poor man's one

--- task4/iot/apps/device/lib/device/registry.ex
+++ task4/iot/apps/device/lib/device/registry.ex
@@ -2,7 +2,7 @@ defmodule Device.Registry do
 
   defmodule State do
     defstruct [devices: %{},
-               subscribers: []]
+               subscribers: MapSet.new()]
   end
 
   def start_link(options \\ []) do
@@ -12,7 +12,7 @@ defmodule Device.Registry do
   def add({device_id, device}) do
     Agent.update(__MODULE__, fn state ->
       new = update_in(state.devices, &Map.put(&1, device_id, device))
-      for {pid} <- new.subscribers do
+      for pid <- new.subscribers do
         send(pid, {:add_device, device_id, device})
       end
       new
@@ -22,7 +22,7 @@ defmodule Device.Registry do
   def remove(device_id) do
     Agent.update(__MODULE__, fn state ->
       new = update_in(state.devices, &Map.delete(&1, device_id))
-      for {pid} <- new.subscribers do
+      for pid <- new.subscribers do
         send(pid, {:remove_device, device_id})
       end
       new
@@ -40,13 +40,13 @@ defmodule Device.Registry do
       for {device_id, device} <- Map.to_list(state.devices) do
         send(pid, {:add_device, device_id, device})
       end
-      update_in(state.subscribers, &List.keystore(&1, pid, 1, {pid}))
+      update_in(state.subscribers, &MapSet.put(&1, pid))
     end)
   end
 
   def unsubscribe(pid) do
     Agent.update(__MODULE__, fn state ->
-      update_in(state.subscribers, &List.keydelete(&1, pid, 1))
+      update_in(state.subscribers, &MapSet.delete(&1, pid))
     end)
   end
 
